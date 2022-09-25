0005bdb5f7d268a63c480ce0f187895e64b89484
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-25 00:04:37 -0500

    [ADD] user information

--- src/app/views/home/app.home.component.ts
+++ src/app/views/home/app.home.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { TrackService } from '../../services/track.service';
 import { HttpHeaders } from '@angular/common/http';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
+import { UserProfileResponseModel } from '../../models/user-profile-response.model';
+import { UserTracksResponseModel } from '../../models/user-tracks-response.model';
+import { playlistItem } from '../../models/user-playlists-response.model';
 
 @Component({
     selector: 'app-home-component',
     templateUrl: './app.home.component.html',
+    styleUrls: ['./app.home.component.css'],
 })
 export class HomeComponent implements OnInit {
     public userToken: string | null = null;
+    public userTracks!: Array<UserTracksResponseModel>;
+    public userPlaylists!: playlistItem[];
 
-    constructor(private _trackService: TrackService) {}
+    constructor(
+        private _trackService: TrackService,
+        private _userService: UserService,
+        private _authService: AuthService
+    ) {}
 
-    ngOnInit(): void {
+    async ngOnInit() {
         if (localStorage.getItem('token')) {
             this.userToken = localStorage.getItem('token') || null;
 
@@ -22,11 +34,42 @@ export class HomeComponent implements OnInit {
 
             const options = { headers };
 
-            console.log(options);
+            this.getUserProfile(options);
 
-            this._trackService.getUserTracks(options).subscribe((resp) => {
-                console.log(resp);
-            });
+            this.getLatestTracks(options, 5);
+
+            this.getUserPlaylists(options);
         }
+    }
+
+    public getUserProfile(options: Object) {
+        this._userService.getUserProfile(options).subscribe((res) => {
+            console.log('user profile');
+            console.log(res);
+            this._userService.userInfo = res;
+        });
+    }
+
+    public getLatestTracks(options: Object, limit: number) {
+        this._trackService.getUserTracks(options, limit).subscribe((resp) => {
+            console.log(resp);
+            this.userTracks = resp.items;
+        });
+    }
+
+    public getUserPlaylists(options: Object) {
+        this._userService.getUserPlaylists(options).subscribe((resp) => {
+            console.log('playlists');
+            console.log(resp);
+            this.userPlaylists = resp.items;
+        });
+    }
+
+    public get isLogin(): boolean {
+        return this._authService.isLogin;
+    }
+
+    public get userInfo(): UserProfileResponseModel {
+        return this._userService.userInfo;
     }
 }

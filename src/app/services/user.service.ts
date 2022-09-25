32b5ceed98fa13f74927d0e5d64e8ecf0968d4b7
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-25 00:01:08 -0500

    [ADD] user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { UserProfileResponseModel } from '../models/user-profile-response.model';
+import { UserPlaylistsResponseModel } from '../models/user-playlists-response.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    private _spotifyApiUrl = environment.spotifyApiUrl;
+
+    public userInfo!: UserProfileResponseModel;
+
+    constructor(private _http: HttpClient) {}
+
+    public getUserProfile(
+        options: Object
+    ): Observable<UserProfileResponseModel> {
+        return this._http.get<UserProfileResponseModel>(
+            `${this._spotifyApiUrl}/me`,
+            options
+        );
+    }
+
+    public getUserPlaylists(
+        options: Object
+    ): Observable<UserPlaylistsResponseModel> {
+        return this._http.get<UserPlaylistsResponseModel>(
+            `${this._spotifyApiUrl}/me/playlists`,
+            options
+        );
+    }
+}

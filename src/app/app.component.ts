77b926dedd0b81bea74c52ae5cd6ae76818b50dd
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:59:56 -0500

    [DELETE] tooglePlayer function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerService } from './services/player.service';
 import { HttpHeaders } from '@angular/common/http';
 import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
     selector: 'app-root',
@@ -13,36 +14,11 @@ export class AppComponent implements OnInit {
 
     constructor(
         private _playerService: PlayerService,
-        private _userService: UserService
+        private _userService: UserService,
+        private _authService: AuthService
     ) {}
 
-    ngOnInit(): void {}
-
-    tooglePlayer() {
-        const body = {};
-
-        const headers = new HttpHeaders({
-            Authorization: this._userService.userToken || '',
-            'Content-Type': 'application/json',
-        });
-        const options = { headers };
-        console.log(body);
-        console.log(options);
-        console.log(this._userService.userToken);
-        this._playerService
-            .startOrResumePlayer(
-                {
-                    position_ms: 0,
-                },
-                {
-                    headers: new HttpHeaders({
-                        Authorization: this._userService.userToken || '',
-                        'Content-Type': 'application/json',
-                    }),
-                }
-            )
-            .subscribe((res) => {
-                console.log(res);
-            });
+    ngOnInit(): void {
+        console.log(this._userService);
     }
 }

6478aacbd16c2b2d9a3f849d13ded8f6684f441d
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:58:43 -0500

    [UPD] spotify scope

--- src/app/components/navbar/app-navbar.component.ts
+++ src/app/components/navbar/app-navbar.component.ts
@@ -33,7 +33,7 @@ export class NavbarComponent implements OnInit {
 
     public spotifyRedirect() {
         const scope =
-            'user-read-private user-read-email playlist-read-private user-library-modify user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing streaming user-read-recently-played user-library-read';
+            'user-read-private user-read-email playlist-read-private user-library-modify user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing streaming user-read-recently-played user-library-read user-follow-read';
 
         window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${environment.spotifyClientId}&scope=${scope}&redirect_uri=${this._redirectUri}&state=qweasdrftgcvdfes`;
     }

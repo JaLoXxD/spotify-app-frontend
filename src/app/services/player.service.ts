0a3be18a8a9408b3fd5bd83743b06c6a252aff8b
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:57:09 -0500

    [ADD] player endpoints

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { PlaybackStateResponse } from '../models/player/playback-state-response.model';
+import { AvailableDevicesResponse } from '../models/player/available-devices-response.model';
 
 @Injectable({
     providedIn: 'root',
@@ -11,10 +13,31 @@ export class PlayerService {
 
     constructor(private _http: HttpClient) {}
 
-    startOrResumePlayer(body: any, options: Object): Observable<any> {
-        console.log('enter');
+    getPlaybackState(
+        options: Object
+    ): Observable<PlaybackStateResponse | null> {
+        return this._http.get<PlaybackStateResponse | null>(
+            `${this._spotifyApiUrl}/me/player`,
+            options
+        );
+    }
+
+    getAvailableDevices(
+        options: Object
+    ): Observable<AvailableDevicesResponse | null> {
+        return this._http.get<AvailableDevicesResponse | null>(
+            `${this._spotifyApiUrl}/me/player/devices`,
+            options
+        );
+    }
+
+    startOrResumePlayer(
+        deviceId: string,
+        body: any,
+        options: Object
+    ): Observable<any> {
         return this._http.put(
-            `${this._spotifyApiUrl}/me/player/play`,
+            `${this._spotifyApiUrl}/me/player/play?device_id=${deviceId}`,
             body,
             options
         );

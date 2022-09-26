9711a5e3029589f05cd8d9e74f0bccc058e5bb97
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:58:59 -0500

    [ADD] component and integrate service

--- src/app/components/latest-tracks/latest-tracks.component.ts
+++ src/app/components/latest-tracks/latest-tracks.component.ts
@@ -1,23 +1,48 @@
 import { UserTracksResponseModel } from '../../models/user-tracks-response.model';
-import {
-    Component,
-    Input,
-    OnInit,
-    OnChanges,
-    SimpleChanges,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { TrackService } from '../../services/track.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
     selector: 'app-latest-tracks',
     templateUrl: './latest-tracks.component.html',
     styleUrls: ['./latest-tracks.component.css'],
 })
-export class LatestTracksComponent implements OnInit, OnChanges {
-    @Input() tracks: UserTracksResponseModel[] = [];
+export class LatestTracksComponent implements OnInit {
+    public tracks!: Array<UserTracksResponseModel>;
 
-    ngOnInit(): void {}
+    constructor(
+        private _trackService: TrackService,
+        private _userService: UserService
+    ) {}
 
-    ngOnChanges(): void {
-        console.log(this.tracks);
+    ngOnInit(): void {
+        if (localStorage.getItem('token')) {
+            const headers = new HttpHeaders({
+                Authorization: this._userService.userToken || '',
+                'Content-Type': 'application/json',
+            });
+
+            const options = { headers };
+            this.getLatestTracks(options, 5);
+        }
+    }
+
+    public getLatestTracks(options: Object, limit: number) {
+        this._trackService.getUserTracks(options, limit).subscribe((resp) => {
+            console.log(resp);
+            this.tracks = resp.items;
+        });
+    }
+
+    public calcTrackDuration(duration: number) {
+        const minutes = Math.floor(duration / 60000);
+        const seconds = Math.round(duration / 1000 - minutes * 60);
+        return `${minutes}:${this.formatDuration(seconds)}`;
+    }
+
+    public formatDuration(time: number) {
+        return time < 10 ? `0${time}` : time;
     }
 }

7531d751d407967c6f02c21cb8a6910d57118dfb
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:58:04 -0500

    [UPD] delete input

--- src/app/components/playlists/grid/playlists-grid.component.ts
+++ src/app/components/playlists/grid/playlists-grid.component.ts
@@ -1,17 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { playlistItem } from '../../../models/user-playlists-response.model';
+import { UserService } from '../../../services/user.service';
+import { HttpHeaders } from '@angular/common/http';
 @Component({
     selector: 'app-playlists-grid',
     templateUrl: './playlists-grid.component.html',
     styleUrls: ['./playlists-grid.component.css'],
 })
 export class PlaylistsGridComponent implements OnInit {
-    @Input() playlists: playlistItem[] = [];
+    public playlists: playlistItem[] = [];
 
     public placeholderPlaylistImg: string =
         'https://via.placeholder.com/450x350';
 
+    constructor(private _userService: UserService) {}
+
     ngOnInit(): void {
-        console.log(this.playlists);
+        if (localStorage.getItem('token')) {
+            const headers = new HttpHeaders({
+                Authorization: this._userService.userToken || '',
+                'Content-Type': 'application/json',
+            });
+
+            const options = { headers };
+            this.getUserPlaylists(options, 5);
+        }
+    }
+
+    public getUserPlaylists(options: Object, limit: number) {
+        this._userService.getUserPlaylists(options, limit).subscribe((resp) => {
+            console.log('playlists');
+            console.log(resp);
+            this.playlists = resp.items;
+        });
     }
 }

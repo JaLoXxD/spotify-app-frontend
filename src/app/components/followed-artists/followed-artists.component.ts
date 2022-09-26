f1f7f4c3f6eb760fb8520899ed1cc34d79615fa3
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-26 17:59:13 -0500

    [ADD] component and integrate service

---
+++ src/app/components/followed-artists/followed-artists.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { ArtistsItem } from '../../models/user-followed-artists-response.model';
+
+@Component({
+    selector: 'app-followed-artists',
+    templateUrl: './followed-artists.component.html',
+    styleUrls: ['./followed-artists.component.css'],
+})
+export class FollowedArtists implements OnInit {
+    @Output() totalFollowedArtists: EventEmitter<number> =
+        new EventEmitter<number>();
+
+    public followedArtists!: ArtistsItem[];
+    public placeholderArtistImg: string = 'https://via.placeholder.com/450x350';
+
+    constructor(private _userService: UserService) {}
+
+    ngOnInit(): void {
+        if (localStorage.getItem('token')) {
+            const headers = new HttpHeaders({
+                Authorization: this._userService.userToken || '',
+                'Content-Type': 'application/json',
+            });
+
+            const options = { headers };
+            this.getFollowedArtists(options);
+        }
+    }
+
+    getFollowedArtists(options: Object) {
+        this._userService
+            .getUserFollowedArtists(options)
+            .pipe(
+                map((resp) => {
+                    return resp.artists;
+                })
+            )
+            .subscribe({
+                next: (resp) => {
+                    console.log(resp);
+                    this.sendTotalFollowedArtists(resp.total);
+                    this.followedArtists = resp.items;
+                },
+            });
+    }
+
+    sendTotalFollowedArtists(total: number) {
+        this.totalFollowedArtists.emit(total);
+    }
+}

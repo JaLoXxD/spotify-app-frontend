449ff7109c2307cc9c74e606fc812aca23438590
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-30 17:47:46 -0500

    [ADD] get userbyid

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -45,4 +45,8 @@ export class UserService {
             options
         );
     }
+
+    public getUserById(userId:string, options: Object): Observable<any> {
+        return this._http.get<any>(`${this._spotifyApiUrl}/users/${userId}`)
+    }
 }

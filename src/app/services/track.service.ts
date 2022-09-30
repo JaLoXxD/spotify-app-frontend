ce8a693182f5d1b9e4869e651cae89c769baee14
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-30 17:47:22 -0500

    [UPD] observable type

--- src/app/services/track.service.ts
+++ src/app/services/track.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TracksResponse } from '../models/tracks-response.model';
 
 @Injectable({
     providedIn: 'root',
@@ -10,7 +12,7 @@ export class TrackService {
 
     constructor(private _http: HttpClient) {}
 
-    public getUserTracks(options: Object, limit: number) {
-        return this._http.get<any>(`${this._spotifyApiUrl}/me/tracks?limit=${limit}`, options);
+    public getUserTracks(options: Object, limit: number): Observable<TracksResponse> {
+        return this._http.get<TracksResponse>(`${this._spotifyApiUrl}/me/tracks?limit=${limit}`, options);
     }
 }

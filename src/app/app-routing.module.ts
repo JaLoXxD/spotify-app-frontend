3f70da136ef8c19e2fc2e4cba1151b526b8e6eff
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-09-30 17:43:14 -0500

    [ADD] playlist route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/app.home.component';
+import { PlaylistComponent } from './views/playlist/playlist.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'playlist/:id', component: PlaylistComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}

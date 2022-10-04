09d6ac1d6a9ac58babe30eeb0eb4b602a648a190
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-04 17:47:14 -0500

    [ADD] login view

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit{
+    private _redirectUri: string = environment.redirectUri;
+    private _code: string = '';
+
+    constructor(
+        private _authService: AuthService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {}
+
+    ngOnInit() {
+
+    }
+
+    public spotifyRedirect() {
+        const scope =
+            'user-read-private user-read-email playlist-read-private user-library-modify user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing streaming user-read-recently-played user-library-read user-follow-read';
+
+        window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${environment.spotifyClientId}&scope=${scope}&redirect_uri=${this._redirectUri}&state=qweasdrftgcvdfes`;
+    }
+}

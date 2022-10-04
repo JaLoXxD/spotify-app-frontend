0aa7ce30d053a4c8bd8abe713cf21a0ec6a724dd
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-04 17:50:19 -0500

    [ADD] token functions and logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     private _spotifyApiUrl: string = environment.spotifyAccountApiUrl;
 
     public isLogin: boolean = false;
+    public expiration: Date | undefined;
 
-    constructor(private _http: HttpClient) {}
+    constructor(private _http: HttpClient, private _router: Router) {}
 
     public spotifyLogin(
         body: URLSearchParams,
@@ -20,5 +22,27 @@ export class AuthService {
             body,
             options
         );
+    }
+
+    public setTokenExpirationDate(expiration:number){
+        const auxDate = new Date();
+        const expirationTime = expiration / 3600;
+        auxDate.setTime(auxDate.getTime() + expirationTime * 60 * 60 * 1000);
+        this.expiration = auxDate;
+        console.log(this.expiration);
+    }
+    
+    public checkIfTokenIsExpired(): boolean{
+        if(new Date() > this.expiration!){
+            return false;
+        }
+        this.spotifyLogout();
+        return true;
+    }
+
+    public spotifyLogout(){
+        localStorage.removeItem('token');
+        this.isLogin = false;
+        this._router.navigate(['/login']);
     }
 }

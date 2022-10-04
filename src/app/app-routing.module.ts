517bb03de404ea92ef4aa7a86ec1955dab8ff9e8
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-04 17:52:40 -0500

    [ADD] guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/app.home.component';
 import { PlaylistComponent } from './views/playlist/playlist.component';
+import { LoginComponent } from './views/login/login.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'playlist/:id', component: PlaylistComponent },
+    { path: '', component: HomeComponent, canActivate:[AuthGuard] },
+    { path: 'login', component: LoginComponent},
+    { path: 'playlist/:id', component: PlaylistComponent, canActivate:[AuthGuard] },
 ];
 
 @NgModule({

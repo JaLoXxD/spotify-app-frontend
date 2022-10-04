10052662a3830a13e3238eb288bf88500e25a5b2
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-04 17:50:47 -0500

    [ADD] auth interceptor

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+    providedIn:'root',
+})
+export class AuthInterceptorService implements HttpInterceptor{
+    constructor(private _router: Router, private _authService: AuthService){}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token: string | null = localStorage.getItem('token');
+        if(token){
+            this._authService.isLogin = true;
+        }
+        return next.handle(req).pipe(
+            catchError((err: HttpErrorResponse)=>{
+                if(err.status === 401){
+                    this._authService.spotifyLogout();
+                }
+                return throwError(()=>{
+                    new Error('Token expirado.')
+                });
+            })
+        )
+    }
+}

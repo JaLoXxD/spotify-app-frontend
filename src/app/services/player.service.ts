199f3f598d2d7317102629bfdd88e343d65ecb41
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-05 15:15:43 -0500

    [ADD] spotify playback functions

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PlaybackStateResponse } from '../models/player/playback-state-response.model';
 import { AvailableDevicesResponse } from '../models/player/available-devices-response.model';
+import { PlayerTrackResponse } from '../models/player/player-track-response.model';
 
 @Injectable({
     providedIn: 'root',
@@ -11,9 +12,14 @@ import { AvailableDevicesResponse } from '../models/player/available-devices-res
 export class PlayerService {
     private _spotifyApiUrl: string = environment.spotifyApiUrl;
 
+    public player: Spotify.Player | null = null;
+    private _currentTrack!:PlayerTrackResponse;
+    public _position:number = 0;
+    public _duration:number = 0;
+
     constructor(private _http: HttpClient) {}
 
-    getPlaybackState(
+    public getPlaybackState(
         options: Object
     ): Observable<PlaybackStateResponse | null> {
         return this._http.get<PlaybackStateResponse | null>(
@@ -22,7 +28,7 @@ export class PlayerService {
         );
     }
 
-    getAvailableDevices(
+    public getAvailableDevices(
         options: Object
     ): Observable<AvailableDevicesResponse | null> {
         return this._http.get<AvailableDevicesResponse | null>(
@@ -31,7 +37,7 @@ export class PlayerService {
         );
     }
 
-    startOrResumePlayer(
+    public startOrResumePlayer(
         deviceId: string,
         body: any,
         options: Object
@@ -42,4 +48,69 @@ export class PlayerService {
             options
         );
     }
+
+    public startSpotifyPlayer(access_token: string) {
+        console.log(access_token);
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            console.log('spotify script loaded');
+            this.player = new Spotify.Player({
+                name: 'Jorge player',
+                getOAuthToken: (cb) => {
+                    cb(access_token);
+                },
+                volume: 0.5,
+            });
+            this.player
+                .connect()
+                .then((success) => console.log('Connected:', success))
+                .catch((err) => {
+                    console.log(err);
+                });
+            this._startPlayerListeners();
+        };
+        const script = document.createElement('script');
+        script.src = 'https://sdk.scdn.co/spotify-player.js';
+        document.body.appendChild(script);
+    }
+
+    public destroySpotifyPlayer(){
+        this.player?.disconnect();
+    }
+
+    private _startPlayerListeners() {
+        if (this.player) {
+            this.player.addListener('ready', ({ device_id }) => {
+                console.log(`Ready with Device ID ${device_id}`);
+            });
+
+            this.player.addListener(
+                'player_state_changed',
+                ({ position, duration, track_window: { current_track } }) => {
+                    this._currentTrack = current_track;
+                    this._position = position;
+                    this._duration = duration;
+                    console.log('Currently Playing', current_track);
+                    console.log('Position in Song', position);
+                    console.log('Duration of Song', duration);
+                }
+            );
+        } else {
+            console.log(`player doesn't exists`);
+        }
+    }
+
+    
+    public get currentTrack() : PlayerTrackResponse {
+        return this._currentTrack;
+    }
+    
+    public get position() : number {
+        return this._position;
+    }
+    
+    public get duration() : number {
+        return this._duration;
+    }
+    
+    
 }

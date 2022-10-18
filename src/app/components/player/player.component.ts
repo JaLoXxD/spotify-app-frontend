b9ca41432aeca9126b8a35ee96c5d8a272f08aad
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-18 17:53:58 -0500

    [ADD] player functions and track timer

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -17,14 +17,15 @@ export class PlayerComponent implements OnInit {
     public devicesContainerClass: boolean = false;
     public deviceIcons: any = {};
     public selectedDevice: Device | null = null;
-    public volume: number = 50;
     public currentTrack!: PlayerTrackResponse | undefined;
-    public name:string | undefined = 'empty';
+    public name: string | undefined = 'empty';
+    public trackTimer: ReturnType<typeof setInterval> | null = null;
+    public trackProgress: number = 0;
 
     constructor(
         public _playerService: PlayerService,
         private _userService: UserService,
-        private ref: ChangeDetectorRef
+        private _ref: ChangeDetectorRef
     ) {}
 
     ngOnInit(): void {
@@ -48,8 +49,41 @@ export class PlayerComponent implements OnInit {
             this.currentTrack = track;
             this.name = track?.name;
             console.log(this.currentTrack);
-            this.ref.detectChanges();
+            console.log(this._playerService.repeatMode);
+            console.log(this._playerService.shuffle);
+            if(this._playerService.duration > 0 && this._playerService.duration === this._playerService.position && this._playerService.repeatMode === 0 && !this._playerService.shuffle){
+                this.pausePlayer();
+            }
+            if (this.currentTrack) {
+                console.log('enter');
+                this.createTrackTimer();
+            }
+            this._ref.detectChanges();
         });
+    }
+
+    createTrackTimer() {
+        this.stopTrackTimer();
+        this.trackTimer = setInterval(() => {
+            this._playerService.trackPosition =
+            this._playerService.calculateTime(this._playerService.position);
+            console.log(this._playerService.trackPosition);
+            console.log(this._playerService.trackDuration);
+            this._playerService.position += 1000;
+            this.calcTrackProgress();
+            this._ref.detectChanges();
+        }, 1000);
+        console.log(this.trackTimer);
+    }
+
+    stopTrackTimer() {
+        clearInterval(this.trackTimer!);
+        this.trackProgress = 0;
+    }
+
+    calcTrackProgress() {
+        this.trackProgress =
+            (this._playerService.position * 100) / this._playerService.duration;
     }
 
     tooglePlayer() {
@@ -77,6 +111,7 @@ export class PlayerComponent implements OnInit {
             .startOrResumePlayer(this.currentDevice, body, options)
             .subscribe({
                 next: (res) => {
+                    this.stopTrackTimer();
                     this.getPlaybackState(options);
                     this._playerService.isPlaying = true;
                 },
@@ -103,6 +138,7 @@ export class PlayerComponent implements OnInit {
             .pausePlayer(this.currentDevice, body, options)
             .subscribe({
                 next: (res) => {
+                    this.stopTrackTimer();
                     this.getPlaybackState(options);
                     this._playerService.isPlaying = false;
                 },
@@ -112,9 +148,57 @@ export class PlayerComponent implements OnInit {
             });
     }
 
+    nextTrack() {
+        const headers = new HttpHeaders({
+            Authorization: this._userService.userToken || '',
+            'Content-Type': 'application/json',
+        });
+        const options = { headers };
+        this._playerService
+            .nextTrack(this.currentDevice, options)
+            .subscribe((resp) => {
+                console.log(resp);
+            });
+    }
+
+    previousTrack() {
+        const headers = new HttpHeaders({
+            Authorization: this._userService.userToken || '',
+            'Content-Type': 'application/json',
+        });
+        const options = { headers };
+        this._playerService
+            .previousTrack(this.currentDevice, options)
+            .subscribe((resp) => {
+                console.log(resp);
+            });
+    }
+
+    toogleRepeatMode() {
+        const headers = new HttpHeaders({
+            Authorization: this._userService.userToken || '',
+            'Content-Type': 'application/json',
+        });
+        const options = { headers };
+        const state: string =
+            this.repeatState === 'off'
+                ? 'context'
+                : this.repeatState === 'context'
+                ? 'track'
+                : 'off';
+        console.log(state);
+        this._playerService.repeatState = state;
+        this._playerService
+            .toogleRepeatMode(this.currentDevice, state, options)
+            .subscribe((resp) => {
+                console.log(resp);
+            });
+    }
+
     getPlaybackState(options: Object) {
         this._playerService.getPlaybackState(options).subscribe((resp) => {
             this.playbackState = resp;
+            console.log(resp);
         });
     }
 
@@ -123,6 +207,7 @@ export class PlayerComponent implements OnInit {
             this.availableDevices = resp?.devices || null;
             this.selectedDevice =
                 this.availableDevices != null ? this.availableDevices[0] : null;
+            this._ref.detectChanges();
             console.log(this.availableDevices);
         });
     }
@@ -136,11 +221,43 @@ export class PlayerComponent implements OnInit {
         this._playerService.currentDevice = deviceInfo.id;
     }
 
+    getVolume(ev: Event) {
+        const target = ev.target as HTMLInputElement;
+        this._playerService.volume = parseInt(target.value);
+    }
+
+    setVolume() {
+        const headers = new HttpHeaders({
+            Authorization: this._userService.userToken || '',
+            'Content-Type': 'application/json',
+        });
+        const options = { headers };
+        this._playerService
+            .setVolume(this.currentDevice, this.volume, options)
+            .subscribe((resp) => {});
+    }
+
     public get currentDevice(): string | undefined {
         return this._playerService.currentDevice;
     }
 
     public get isPlaying(): boolean {
         return this._playerService.isPlaying;
+    }
+
+    public get repeatState(): string {
+        return this._playerService.repeatState;
+    }
+
+    public get volume(): number {
+        return this._playerService.volume;
+    }
+
+    public get trackDuration(): string {
+        return this._playerService.trackDuration;
+    }
+
+    public get trackPosition(): string {
+        return this._playerService.trackPosition;
     }
 }

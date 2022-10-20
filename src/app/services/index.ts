a5bfe92b40a132569c83d4c3c5a70c16c4436b93
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-20 17:29:32 -0500

    export all services

---
+++ src/app/services/index.ts
@@ -0,0 +1,7 @@
+export * from "./auth-interceptor.service";
+export * from "./auth.guard";
+export * from "./auth.service";
+export * from "./player.service";
+export * from "./playlist.service";
+export * from "./track.service";
+export * from "./user.service";

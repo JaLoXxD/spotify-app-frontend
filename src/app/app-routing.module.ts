5361af13b464581e88032ca2da0926a21ccb809e
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-28 12:33:04 -0500

    [ADD] playlists routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,22 @@ import { HomeComponent } from './views/home/app.home.component';
 import { PlaylistComponent } from './views/playlist/playlist.component';
 import { LoginComponent } from './views/login/login.component';
 import { AuthGuard } from './services/auth.guard';
+import { PlaylistsComponent } from './views/playlists/playlists.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate:[AuthGuard] },
-    { path: 'login', component: LoginComponent},
-    { path: 'playlist/:id', component: PlaylistComponent, canActivate:[AuthGuard] },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'playlists',
+        canActivate: [AuthGuard],
+        children: [
+            { path: '', component: PlaylistsComponent },
+            {
+                path: ':id',
+                component: PlaylistComponent,
+            },
+        ],
+    },
 ];
 
 @NgModule({
